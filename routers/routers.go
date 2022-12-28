3e453793adadbad71eeab8b10652d8349057336e
wiaoj <71731911+wiaoj@users.noreply.github.com>
2022-12-28 13:17:58 +0300

    Revert "Niko dev"

--- routers/routers.go
+++ routers/routers.go
@@ -1,16 +1,17 @@
 package routers
 
 import (
-	"wiaoj/authorization/handlers"
+	"proxolab-ecommerce-authorization/handlers"
 
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
-func Initalize(app *fiber.App, DB *gorm.DB) {
+func SetupRoutes(app *fiber.App, DB *gorm.DB) {
 	h := handlers.New(DB)
 	api := app.Group("/api/v1")
 	auth := api.Group("/auth")
 	auth.Post("/signup", h.Signup)
 	auth.Post("/login", h.Login)
+
 }
--- utils/JWT/jwt.go
+++ utils/JWT/jwt.go
@@ -1,30 +1,13 @@
-package utils
-
-import (
-	"strconv"
-	"time"
-	"wiaoj/authorization/config"
-
-	"github.com/golang-jwt/jwt/v4"
-	"github.com/google/uuid"
-)
-
-func GenerateJwtToken(userID uuid.UUID, Email string, Name string, AccessLevel int) (string, error) {
-	var secretKey = []byte(config.GetJwtSecretKey())
-	var expirationMinutes, err = strconv.Atoi(config.GetJwtExpirationMinutes())
-
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-
-	claims["email"] = Email
-	claims["exp"] = time.Now().Add(time.Minute * time.Duration(expirationMinutes)).Unix()
-	claims["level"] = AccessLevel
-	claims["userId"] = userID
-
+func generateJWT(username string, userId int, accessLevel int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId":      userId,
+		"username":    username,
+		"accessLevel": accessLevel,
+	})
+	secretKey := []byte(config.Config("JWT_SECRET_FOR_LOCAL"))
 	tokenString, err := token.SignedString(secretKey)
-
 	if err != nil {
-		return "", err
+		fmt.Println(err)
 	}
-	return tokenString, nil
+	return tokenString, err
 }
